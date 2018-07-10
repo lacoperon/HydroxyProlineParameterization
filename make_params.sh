008a33081a6ba3c7b594041bed6e28e5cefc040d
lacoperon <ejwilliams@wesleyan.edu>
2018-07-10 14:32:51 -0400

    Fixed typo which led to dihydroxyproline not being made properly

--- make_params.sh
+++ make_params.sh
@@ -21,12 +21,17 @@ cd data
 sed 's/UNK/PRD/g' ejw_dihydroxyproline_draft.pdb > dihydroxypro.pdb
 sed 's/UNK/PRT/g' ejw_trans3hydroxyproline_draft.pdb > trans3pro.pdb
 
+# STEP 2. Add hydrogens using 'reduce'
+
+/share/apps/CENTOS6/amber/amber16/bin/reduce -FLIP dihydroxypro.pdb > dihydroxypro_h.pdb
+/share/apps/CENTOS6/amber/amber16/bin/reduce -FLIP trans3pro.pdb > trans3pro_h.pdb
+
 # STEP 2. Use `antechamber` to parameterize the AMBER force constants
 # and remove all antechamber metafiles. We output the parameters in
 # Antechamber (.ac) format, with AMBER atom formatting.
 # (As seen in http://ambermd.org/tutorials/basic/tutorial4b/, with modifications)
-antechamber -i dihydroxypro.pdb -fi pdb -o dihydroxypro.ac -fo ac -c bcc -at amber
-antechamber -i trans3pro.pdb    -fi pdb -o trans3pro.ac    -fo ac -c bcc -at amber
+antechamber -i dihydroxypro_h.pdb -fi pdb -o dihydroxypro.ac -fo ac -c bcc -at amber
+antechamber -i trans3pro_h.pdb    -fi pdb -o trans3pro.ac    -fo ac -c bcc -at amber
 rm sqm*
 rm ANTECHAMBER*
 rm ATOMTYPE.INF
@@ -39,7 +44,7 @@ parmchk -i dihydroxypro.ac -f ac -o dihydroxypro.frcmod
 
 # HEAD_NAME        -  the N-terminal atom name to connect
 # TAIL_NAME        -  C terminal atom name to connect
-# MAIN_CHAIN       -  a main chain atom. 
+# MAIN_CHAIN       -  a main chain atom.
 # OMIT_NAME        - atoms to delete in the bonded residue
 # PRE_HEAD_TYPE    - atom type to which the head atom bonds in previous residue
 # POST_TAIL_TYPE   - atop type to which tail atom bonds in next residue
@@ -49,7 +54,8 @@ cat <<EOF > trans3pro.mc
 HEAD_NAME N
 TAIL_NAME C4
 MAIN_CHAIN C
-OMIT_NAME H4
+OMIT_NAME H8
+OMIT_NAME O1
 OMIT_NAME H
 PRE_HEAD_TYPE C
 POST_TAIL_TYPE N
@@ -60,7 +66,8 @@ cat <<EOF > dihydroxypro.mc
 HEAD_NAME N
 TAIL_NAME C4
 MAIN_CHAIN C
-OMIT_NAME H4
+OMIT_NAME H8
+OMIT_NAME O1
 OMIT_NAME H
 PRE_HEAD_TYPE C
 POST_TAIL_TYPE N
@@ -70,10 +77,10 @@ EOF
 # STEP 5. Run `prepgen` to get prep input files
 # (Note that we're also naming our residues here)
 
-prepgen -i trans3pro.ac    -o trans3pro.prepin    -m trans3pro.mc    -rn PR3
+prepgen -i trans3pro.ac    -o trans3pro.prepin    -m trans3pro.mc    -rn PRT
 prepgen -i dihydroxypro.ac -o dihydroxypro.prepin -m dihydroxypro.mc -rn PRD
 
-# STEP 6. Run `parmchk2` to get the `frcmod` files 
+# STEP 6. Run `parmchk2` to get the `frcmod` files
 # We will use these files to 'plug in' our derived force constants,
 # for each of the two modified residues we're adding to our system
 
